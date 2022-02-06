6ef6b033ebd78d6a4b5347a53784210958168a97
FSallust <cecco_96@yahoo.it>
2022-02-06 23:34:26 +0100

    app name changed

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'frontend',
+  appName: 'TKD 2.0',
   webDir: 'www',
   bundledWebRuntime: false
 };
